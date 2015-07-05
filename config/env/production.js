ef5d8a3d4ba3462822f1af87a70049ca2d8d2d56
Ian Simmons <isimmons33@gmail.com>
2015-07-05 19:12:16 -0400

    add production env config file

---
+++ config/env/production.js
@@ -0,0 +1,28 @@
+module.exports = {
+    //development specific env config settings
+
+    /** Database **/
+    //example non local mongodb://username:password@hostname:port/database
+    db: process.env.DATABASE,
+
+    /** Sessions **/
+    sessionSecret: process.env.SESSION_SECRET,
+
+    facebook: {
+        clientID: process.env.FB_CLIENT_ID,
+        clientSecret: process.env.FB_CLIENT_SECRET,
+        callbackURL: process.env.FB_CALLBACK_URL
+    },
+
+    twitter: {
+        clientID: process.env.TW_CLIENT_ID,
+        clientSecret: process.env.TW_CLIENT_SECRET,
+        callbackURL: process.env.TW_CALLBACK_URL
+    },
+
+    google: {
+        clientID: process.env.G_CLIENT_ID,
+        clientSecret: process.env.G_CLIENT_SECRET,
+        callbackURL: process.env.G_CALLBACK_URL
+    }
+};
