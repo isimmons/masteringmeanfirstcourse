81b41c76c94893909283c59c361276b085796c7c
Ian Simmons <isimmons33@gmail.com>
2015-07-06 11:31:31 -0400

    note on reason for validators dir and file

--- app/validators/user.server.validator.js
+++ app/validators/user.server.validator.js
@@ -1,3 +1,6 @@
+//this file not in use at the moment.
+//looking into cleaning things up by moving validation to a set of
+//validator functions in this separate file for use in the model schema
 module.exports = {
     passwordValidator: function(password) {
         return password.length >= 6;
